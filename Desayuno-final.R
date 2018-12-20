f91a26e602e305bf3e2dfe37fbbb4a4db8cb371e
BarbaraBlue <44910296+BarbaraBlue@users.noreply.github.com>
2018-12-19 22:23:58 -0300

    Update Desayuno-final.R

--- Desayuno-final.R
+++ Desayuno-final.R
@@ -1,11 +1,11 @@
 #Establecer carpeta "Tarea_Desayuno" como directorio de trabajo.
 setwd("~/Tarea_Desayuno")
 
-#Cortar: strig -> string
-#def Cortar: si x es ("pan") el resultado será ("pan cortado")
-#si x es ("naranajas") el resultado será ("naranjas cortadas")
-#si x es ("palta") el resultado será ("palta cortada")
-#si es otra cosa el resultado será ("No sé qué hacer con",x)
+#Cortar: string -> string
+#def Cortar: si x es ("pan") el resultado serÃ¡ ("pan cortado")
+#si x es ("naranjas") el resultado serÃ¡ ("naranjas cortadas")
+#si x es ("palta") el resultado serÃ¡ ("palta cortada")
+#si es otra cosa el resultado serÃ¡ ("No sÃ© quÃ© hacer con",x)
 #Ejemplo: Cortar("naranjas")->("naranjas cortadas")
 
 Cortar <- function(x){
@@ -16,46 +16,46 @@ Cortar <- function(x){
   }else if(x=="palta"){
     return("palta cortada")
   }else{
-    return(paste("No sÃ© que hacer con ",x))
+    return(paste("No sÃƒÂ© que hacer con ",x))
   }
 }
 
 #Moler: string -> string
-#def Moler: si x es ("palta cortada") el resultado será ("palta molida")
-#si es otra cosa el resultado será ("No sé qué hacer con ",x)
+#def Moler: si x es ("palta cortada") el resultado serÃ¡ ("palta molida")
+#si es otra cosa el resultado serÃ¡ ("No sÃ© quÃ© hacer con ",x)
 #Ejemplo: Moler("palta cortada")->("palta molida")
 
 Moler <- function(x){
   if(x=="palta cortada"){
     return("palta molida")
   }else{
-    return(paste("No sÃ© que hacer con ",x))
+    return(paste("No sÃƒÂ© que hacer con ",x))
   }
 }
 
 #CalentarSarten: string -> string
-#def CalentarSarten: resultado ("sartén caliente")
-#Ejemplo: CalentarSarten()->("sartén caliente")
+#def CalentarSarten: resultado ("sartÃ©n caliente")
+#Ejemplo: CalentarSarten()->("sartÃ©n caliente")
 
 CalentarSarten <- function(){
-  return("sartÃ©n caliente")
+  return("sartÃƒÂ©n caliente")
 }
 
 #Preparar: string string -> string
-#def Preparar: si x es "naranjas cortadas" el resultado será "jugo de naranjas"
-#si (x,y) es ("masa de panqueques","sartén caliente") 
-#ó es ("sartén caliente","masa de panqueques") el resultado será ("panqueques")
-#si (x,y) es ("harina","huevos") ó ("huevos","harina")
-#el resultado será ("masa de panqueques")
+#def Preparar: si x es "naranjas cortadas" el resultado serÃ¡ "jugo de naranjas"
+#si (x,y) es ("masa de panqueques","sartÃ©n caliente") 
+#Ã³ es ("sartÃ©n caliente","masa de panqueques") el resultado serÃ¡ ("panqueques")
+#si (x,y) es ("harina","huevos") Ã³ ("huevos","harina")
+#el resultado serÃ¡ ("masa de panqueques")
 #si (x,y) es ("pan tostado","palta molida") o ("palta molida","pan tostado") 
-#el resultado será ("pan tostado con palta")
-#si es otra cosa el resultado será ("faltan elementos")
+#el resultado serÃ¡ ("pan tostado con palta")
+#si es otra cosa el resultado serÃ¡ ("faltan elementos")
 #Ejemplo: Preparar("harina","huevos")->("masa de panqueques")
 
 Preparar <- function(x,y){
   if(x=="naranjas cortadas"){
     return("jugo de naranjas")
-  }else if((x=="masa de panqueques" && y=="sartÃ©n caliente")||(y=="masa de panqueques" && x=="sartÃ©n caliente")){
+  }else if((x=="masa de panqueques" && y=="sartÃƒÂ©n caliente")||(y=="masa de panqueques" && x=="sartÃƒÂ©n caliente")){
     return("panqueques")
   }else if((x=="harina" && y=="huevos")||(y=="harina" && x=="huevos")){
     return("masa de panqueques")
@@ -69,7 +69,7 @@ Preparar <- function(x,y){
 #Calentar: string -> string
 #def Calentar: si x es "cafe" el resultado es "cafe caliente"
 #si x es "pan cortado" el resultado es "pan tostado"
-#y si x es otra cosa el resultado será "No sé qué hacer con x"
+#y si x es otra cosa el resultado serÃ¡ "No sÃ© quÃ© hacer con x"
 #Ejemplo: Calentar("cafe")->"cafe caliente"
 
 Calentar <- function(x){
@@ -78,34 +78,34 @@ Calentar <- function(x){
   }else if(x=="pan cortado"){
     return("pan tostado")
   }else{
-    return(paste("No sÃ© que hacer con ",x))
+    return(paste("No sÃƒÂ© que hacer con ",x))
   }
 }
 
 #Rellenar: string -> string
-#def Rellenar: si x es ("panqueques") el resultado será ("panqueques con manjar")
-#si es otra cosa el resultado será ("no sé qué hacer con ",x)
+#def Rellenar: si x es ("panqueques") el resultado serÃ¡ ("panqueques con manjar")
+#si es otra cosa el resultado serÃ¡ ("no sÃ© quÃ© hacer con ",x)
 #Ejemplo: Rellenar("panqueques")->("panqueques rellenos con manjar")
 
 Rellenar <- function(x){
   if(x=="panqueques"){
     return("panqueques con manjar")
   }else{
-    return(paste("No sÃ© que hacer con ",x)) 
+    return(paste("No sÃƒÂ© que hacer con ",x)) 
   }
 }
 
 #Servir: string string string string -> string
-#def Servir: si (x,y,z,j) son ("pan tostado con palta","panqueques con manjar","jugo de naranja","café caliente") 
-#el resultado será ("mmmmm... niami... el desayuno está maravilloso")
-#sino el resultado será ("iiiuuuu... esto no está listo")
-#Ejemplo: Servir("pan tostado con palta","panqueques con manjar","jugo de naranja", "café caliente")->("mmmmm... niami... el desayuno estÃ¡ maravilloso")
+#def Servir: si (x,y,z,j) son ("pan tostado con palta","panqueques con manjar","jugo de naranja","cafÃ© caliente") 
+#el resultado serÃ¡ ("mmmmm... niami... el desayuno estÃ¡ maravilloso")
+#sino el resultado serÃ¡ ("iiiuuuu... esto no estÃ¡ listo")
+#Ejemplo: Servir("pan tostado con palta","panqueques con manjar","jugo de naranja", "cafÃ© caliente")->("mmmmm... niami... el desayuno estÃƒÂ¡ maravilloso")
 
 Servir <- function(x,y,z,j){
   if((x=="pan tostado con palta" && y=="panqueques con manjar" && z=="jugo de naranjas" && j=="cafe caliente")){
-    return("mmmmm... niami... el desayuno estÃ¡ maravilloso")
+    return("mmmmm... niami... el desayuno estÃƒÂ¡ maravilloso")
   }else{
-    return("iiiuuuu... esto no estÃ¡ listo")
+    return("iiiuuuu... esto no estÃƒÂ¡ listo")
   }
 }
 
